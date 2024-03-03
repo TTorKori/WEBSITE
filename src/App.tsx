7336e26ef5343f178434e92b5a0a221565da53d9
TTorKori <vickegbu@gmail.com>
2024-03-03 19:33:03 +0000

    Products Read from Database

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
 import "./css/App.css";
 import Title from "./components/Title";
-import products from "./assets";
 import supabaseClient from "./config/supabase";
 import LoginButton from "./components/LoginButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductShop from "./components/ProductShop";
 import ProductWindow from "./components/ProductWindow";
-import Display from "./components/Display";
 
 function App() {
-  console.log(supabaseClient);
+
+  const [fetchError, setFetchError] = useState<string>("");
+  const [products, setProducts] = useState<any[]>([]);
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const [showProductShop, setShowProductShop] = useState(false);
   const [showProductWindow, setShowProductWindow] = useState(false);
   const [currentProductInView, setCurrentProductInView] = useState("");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const {data, error} = await supabaseClient
+        .from("products")
+        .select();
+
+        if(error) {
+          setFetchError("Could not find products in database");
+          setProducts([]);
+          console.log(error);
+        }
+    
+        if(data){
+          setProducts(data);
+          setFetchError("");
+        }
+
+    }
+
+    fetchProducts();
+  }, []);
 
   const handleLogIn = () => {
     setIsLoggedIn(!isLoggedIn);
@@ -36,19 +58,22 @@ function App() {
     setShowProductWindow(false);
   }
 
+  console.log(products);
   return (
     <div className="app">
+      {fetchError && <p>{`Could not find products ${fetchError}`}</p>}
+      {products.length === 0 && <p>No products in the shop</p>}
       <Title />
       <LoginButton onClick={handleLogIn}>
         {isLoggedIn ? "LOG OUT" : "LOGIN"}
       </LoginButton>
 
-      <Display>
+      <div className="display">
         {showProductShop && isLoggedIn && (
           <ProductShop products={products} onClick={handleProductInteraction} />
         )}
         {showProductWindow && <ProductWindow product={currentProductInView} onReturn={handleReturn}/>}
-      </Display>
+      </div>
     </div>
   );
 }
--- src/components/ProductShop.tsx
+++ src/components/ProductShop.tsx
@@ -1,32 +1,26 @@
 import ProductCard from "./ProductCard";
 
-interface Product {
-  id: string;
-  title: string;
-  images: string[];
-}
-
 interface Props {
-  products: Product[],
+  products: any[],
   onClick: (product: string) => void
 }
 
 function ProductShop({ products, onClick}: Props) {
 
   return (
-    <div className="product-window">
+    <>
       {products.length === 0 && <p>No Products Found.</p>}
       {/* Replace the li with Product Component */}
       {products.map((product) => (
 
         <ProductCard key={product.id} onClick={onClick} productId={product.id}>
 
-          <img src={product.images[0]} alt="" />
+          {/* <img src={product.images[0]} alt="" /> */}
           {product.title}
 
         </ProductCard>
       ))}
-    </div>
+    </>
   );
 }
 
